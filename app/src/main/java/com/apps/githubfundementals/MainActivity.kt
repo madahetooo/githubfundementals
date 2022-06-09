585234da4a0541f7ce5ce925fab2163edd15a9c7
madahetooo <ieslammedhat@gmail.com>
2022-06-09 19:49:25 +0200

    This is our first println method

--- app/src/main/java/com/apps/githubfundementals/MainActivity.kt
+++ app/src/main/java/com/apps/githubfundementals/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.apps.githubfundementals
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("This is our First Commit")
     }
 }
