bcce5d4b96352e7d63abbf008db589d1016721df
madahetooo <ieslammedhat@gmail.com>
2022-06-09 19:54:11 +0200

    This is our second println method

--- app/src/main/java/com/apps/githubfundementals/MainActivity.kt
+++ app/src/main/java/com/apps/githubfundementals/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("This is our First Commit")
+        println("This is our second Commit")
     }
 }
